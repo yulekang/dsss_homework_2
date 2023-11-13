b5c9a1d1e1cd6797fe0604e8c17a8cbcd77771ef
Kang Yule <yule.kang@fau.de>
2023-11-13 18:21:46 +0100

    On branch code_cleanup  Your branch is up to date with 'origin/code_cleanup'.
     Changes to be committed:
    	modified:   math_quiz/math_quiz.py
    	modified:   math_quiz/tests_math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,95 @@
 import random
 
-
-def function_A(min, max):
+def randomInteger(startNumber, endNumber):
     """
-    Random integer. 
+    Random integer.
+    Generate random number between startNumber and endNumber.
+    Parameters
+    ----------
+    startNumber : integer
+        The number of the random to be started.
+    endNumber : integer
+        The number of the random to be ended.
+    Returns
+    -------
+    output : integer
+        An integer random between startNumber and endNumber.
     """
-    return random.randint(min, max)
+    return random.randint(startNumber, endNumber)
 
 
-def function_B():
+def randomOperator():
+    """
+    Random operator.
+    Generate random operator in '+', '-', '*'.
+    output : string
+        One of the operator in '+', '-', '*'.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def computeByOperator(number1, number2, operator):
+    """
+    Compute by operator.
+    Calculate the result of two number through the given operator.
+    Parameters
+    ----------
+    number1 : integer
+        The first number of the calculation.
+    number2 : integer
+        The second number of the calculation.
+    operator : string
+        The operator of the calculation.
+    Returns
+    -------
+    formulaString : string
+        The formula string of the calculation.
+    result : integer
+        The result of the calculation.
+    """
+    formulaString = f"{number1} {operator} {number2}"
+    if operator == '+': result = number1 + number2
+    elif operator == '-': result = number1 - number2
+    else: result = number1 * number2
+    return formulaString, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Generate two random integer and a operator.
+    To compute the correct result, and compare to the user answer.
+    """
+    score = 0
+    questionNumber = 4
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    # The loop for the questionNumber.
+    for _ in range(questionNumber):
+        # Get the two number and the operator.
+        number1 = randomInteger(1, 10)
+        number2 = randomInteger(1, 6)
+        operator = randomOperator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
+        # Generate the string and result of calculation.
+        problem, answer = computeByOperator(number1, number2, operator)
+        print(f"\nQuestion: {problem}")
+        
+        # Get the answer of the user.
+        userAnswer = input("Your answer: ")
+        try:
+            userAnswerInt = int(userAnswer)
+        except:
+            print("Please input integer!")
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            # Compare the correct result and the answer of the user,then compute the score.
+            if userAnswerInt == answer:
+                print("Correct! You earned a point.")
+                score += 1
+            else:
+                print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{questionNumber}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,6 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
-
+from math_quiz import randomInteger, randomOperator, computeByOperator
 
 class TestMathGame(unittest.TestCase):
 
@@ -9,22 +8,32 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = randomInteger(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        # Test if random operator generated are within the specified range 
+        operators = ['+', '-', '*']
+        for _ in range(1000):  # Test the random operator
+            rand_operator = randomOperator()
+            self.assertTrue(rand_operator in operators)
 
     def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        # The test cases
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (8, 4, '-', '8 - 4', 4),
+            (3, 6, '*', '3 * 6', 18),
+            (7, 4, '+', '7 + 4', 11),
+            (3, 6, '-', '3 - 6', -3),
+            (4, 7, '*', '4 * 7', 28)
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            # Get the result of the calculation and adjust.
+            problem, answer = computeByOperator(num1, num2, operator)
+            self.assertTrue(problem == expected_problem)
+            self.assertTrue(answer == expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
